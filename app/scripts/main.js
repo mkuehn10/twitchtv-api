58aac344f829129552e5a1e6d87b81d6b0a958aa
Michael Kuehn <mkuehn10@gmail.com>
2016-08-10 21:05:54 -0400

    basic navbar functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+
+    $(".nav a").on("click", function() {
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+
+    var ViewModel = function() {
+        var self = this;
+
+        self.selection = ko.observable('all');
+
+        self.clickSelection = function(data, event) {
+            console.log(event.target.hash);
+            // filter array by selection?
+        };
+
+        self.refreshStreams = function() {
+            console.log("Loading streams.  .  .")
+        }
+    };
+
+    ko.applyBindings(new ViewModel);
+});
