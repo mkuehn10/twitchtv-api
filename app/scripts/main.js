6e9b5df209d5a7f16d4c142643d2a9de87e71347
Michael Kuehn <mkuehn10@gmail.com>
2016-08-15 00:34:18 -0400

    modify how all/online/offline active class is handled

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,26 @@
 $(function() {
 
-    $(".nav a").on("click", function() {
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-    });
+    // $(".nav a").on("click", function() {
+    //     $(".nav").find(".active").removeClass("active");
+    //     $(this).parent().addClass("active");
+    // });
 
     var ViewModel = function() {
         var self = this;
 
-        self.selection = ko.observable('all');
+        self.selection = ko.observable('#all');
 
         self.clickSelection = function(data, event) {
             console.log(event.target.hash);
+            self.selection(event.target.hash);
             // filter array by selection?
         };
 
         self.refreshStreams = function() {
             console.log("Loading streams.  .  .")
-        }
+        };
+
+        self.refreshStreams();
     };
 
     ko.applyBindings(new ViewModel);
